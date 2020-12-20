8d2fae14e38a349ec0ed1ec86f6ae33cd5924422
Joshua-user <Joshua-user@users.noreply.github.com>
2020-12-20 20:05:48 +0000

    update

---
+++ Crossy Road/Assets/Scripts/DestroyOOB.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOOB : MonoBehaviour
+{
+
+
+    void Start()
+    {
+
+    }
+
+
+    void DestroyObjectDelayed()
+    {
+        // Kills the game object in 5 seconds after loading the object
+        Destroy(gameObject, 5);
+    }
+}
---
+++ Crossy Road/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] Prefabs;
+
+    private float spawnRangeX = 20;
+    private float spawnPosZ = 20;
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
+
+    private void Start()
+    {
+        InvokeRepeating("SpawnRandom", startDelay, spawnInterval);
+    }
+    void SpawnRandom()
+    {
+       
+        {
+            int Index = Random.Range(0, Prefabs.Length);
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+            Instantiate(Prefabs[Index], spawnPos, Prefabs[Index].transform.rotation);
+        
+        }
+
+    }
+}
