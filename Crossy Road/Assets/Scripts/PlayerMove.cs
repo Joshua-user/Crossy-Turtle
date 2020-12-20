574a593abae561ecb3761b3144fb7487e40de25e
Joshua-user <Joshua-user@users.noreply.github.com>
2020-12-20 17:27:55 +0000

    move

---
+++ Crossy Road/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    
+    private CharacterController _controller;
+
+    [SerializeField]
+    private float _movespeed = 5f;
+    [SerializeField]
+    private float _gravity = 9f;
+    [SerializeField]
+    private float _jumpSpeed = 3f;
+
+    private float _directionY;
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+    
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(horizontalInput, 0, verticalInput);
+
+        if(Input.GetButtonDown("jump"))
+        {
+            _directionY = _jumpSpeed;
+        }
+
+        direction.y -= _gravity;
+        direction.y = _directionY;
+
+        _controller.Move(direction *_movespeed * Time.deltaTime);
+    }
+}
